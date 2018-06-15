3e305011d9c3a72d2d39610002d905596b16b5c9
sebas77 <ssense@tiscali.it>
2018-06-15 17:03:02 +0100

    adding more tests

---
+++ Svelto.ECS.Tests/ECSAllocationTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Svelto.ECS;
+
+namespace ECSAllocationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckAllocations().Wait();
+        }
+
+        private static async Task CheckAllocations()
+        {
+            var enginesRoot = new EnginesRoot(new SimpleSubmissionEntityViewScheduler());
+            var factory = enginesRoot.GenerateEntityFactory();
+            int i = 0;
+
+            while (true)
+            {
+                factory.BuildEntity<TestAllocation>(i++, null);
+
+                await Task.Delay(100);
+            }
+        }
+    }
+
+    internal class TestAllocation:GenericEntityDescriptor<EntityStruct>
+    {
+    }
+
+    internal struct EntityStruct : IEntityStruct
+    {
+        public EGID ID { get; set; }
+    }
+}
