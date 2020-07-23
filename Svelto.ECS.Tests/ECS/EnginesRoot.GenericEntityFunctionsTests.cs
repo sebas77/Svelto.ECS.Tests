26744ef2f01ce678aab05a8cf3ed58e999a87187
Jose Luis Rey Mendez <jlreymendez@gmail.com>
2020-07-23 09:52:25 -0300

    Added remove entity tests and added exception checks to swap entity with egid

--- Svelto.ECS.Tests/ECS/EnginesRoot.GenericEntityFunctionsTests.cs
+++ Svelto.ECS.Tests/ECS/EnginesRoot.GenericEntityFunctionsTests.cs
@@ -17,20 +17,83 @@ namespace Svelto.ECS.Tests.ECS
             _enginesRoot.AddEngine(_engine);
         }
 
-        [TearDown]
-        public void Cleanup()
+        // [TearDown]
+        // public void Cleanup()
+        // {
+        //     _functions.RemoveAllEntities<TestEntityWithComponentViewAndComponentStruct>();
+        //     _scheduler.SubmitEntities();
+        // }
+
+        [Test]
+        public void TestRemoveEntityWithEntityIdAndGroup()
         {
-            _functions.RemoveAllEntities<TestEntityWithComponentViewAndComponentStruct>(GroupA);
-            _functions.RemoveAllEntities<TestEntityWithComponentViewAndComponentStruct>(GroupB);
+            CreateTestEntity(0, GroupA);
+            CreateTestEntity(1, GroupA);
             _scheduler.SubmitEntities();
+
+            _functions.RemoveEntity<TestEntityWithComponentViewAndComponentStruct>(0, GroupA);
+            _scheduler.SubmitEntities();
+
+            var exists = _engine.entitiesDB.Exists<TestEntityStruct>(0, GroupA);
+            Assert.IsFalse(exists, "Entity should be removed from target group");
+
+            var count = _engine.entitiesDB.Count<TestEntityStruct>(GroupA);
+            Assert.AreEqual(1, count, "Other entities should not be removed");
+
+            void RemoveEntityNotFound()
+            {
+                _functions.RemoveEntity<TestEntityWithComponentViewAndComponentStruct>(0, GroupA);
+                _scheduler.SubmitEntities();
+            }
+
+            Assert.Throws<ECSException>(RemoveEntityNotFound, "When removing non created entities an exception should be thrown");
+        }
+
+        [Test]
+        public void TestRemoveEntityWithEgid()
+        {
+            CreateTestEntity(0, GroupA);
+            CreateTestEntity(1, GroupA);
+            _scheduler.SubmitEntities();
+
+            var egid = new EGID(0, GroupA);
+            _functions.RemoveEntity<TestEntityWithComponentViewAndComponentStruct>(egid);
+            _scheduler.SubmitEntities();
+
+            var exists = _engine.entitiesDB.Exists<TestEntityStruct>(0, GroupA);
+            Assert.IsFalse(exists, "Entity should be removed from target group");
+
+            var count = _engine.entitiesDB.Count<TestEntityStruct>(GroupA);
+            Assert.AreEqual(1, count, "Other entities should not be removed");
+
+            void RemoveEntityNotFound()
+            {
+                _functions.RemoveEntity<TestEntityWithComponentViewAndComponentStruct>(new EGID(0, GroupA));
+                _scheduler.SubmitEntities();
+            }
+
+            Assert.Throws<ECSException>(RemoveEntityNotFound, "When removing non created entities an exception should be thrown");
+        }
+
+        [Test]
+        public void TestRemoveAllEntitiesWithGroup()
+        {
+            CreateTestEntity(0, GroupA);
+            CreateTestEntity(1, GroupA);
+            CreateTestEntity(2, GroupA);
+            _scheduler.SubmitEntities();
+
+            _functions.RemoveAllEntities<TestEntityWithComponentViewAndComponentStruct>(GroupA);
+            _scheduler.SubmitEntities();
+
+            var countA = _engine.entitiesDB.Count<TestEntityStruct>(GroupA);
+            Assert.AreEqual(0, countA, "Target group should be empty after remove all entities");
         }
 
         [Test]
         public void TestSwapEntityFromEgidToEgid()
         {
-            var initializer = _factory.BuildEntity<TestEntityWithComponentViewAndComponentStruct>
-                (0, GroupA, new object[] {new TestFloatValue(1f), new TestIntValue(1)});
-            initializer.Init(new TestEntityStruct(1f, 1));
+            CreateTestEntity(0, GroupA);
 
             _scheduler.SubmitEntities();
 
@@ -42,12 +105,45 @@ namespace Svelto.ECS.Tests.ECS
 
             var (componentB, componentViewB, countB) = _engine.entitiesDB.QueryEntities<TestEntityStruct, TestEntityViewStruct>(GroupB);
 
-            Assert.AreEqual(1, countB, "An entity should exist in Group B");
+            Assert.AreEqual(1, countB, "An entity should exist in target Group");
             Assert.AreEqual(toEgid.entityID, componentB[0].ID.entityID, "Swapped entity should have the target entityID");
             Assert.AreEqual(1f, componentB[0].floatValue, "Component values should be copied");
             Assert.AreEqual(1, componentB[0].intValue, "Component values should be copied");
             Assert.AreEqual(1f, componentViewB[0].TestFloatValue.Value, "ViewComponent values should be copied");
             Assert.AreEqual(1, componentViewB[0].TestIntValue.Value, "ViewComponent values should be copied");
+
+            var existsA = _engine.entitiesDB.Exists<TestEntityStruct>(0, GroupA);
+            Assert.IsFalse(existsA, "Entity should not be present in source Group anymore");
+
+            void SwapEntityAlreadyExists()
+            {
+                CreateTestEntity(2, GroupA);
+                CreateTestEntity(2, GroupB);
+                fromEgid = new EGID(0, GroupA);
+                toEgid = new EGID(0, GroupB);
+                _functions.SwapEntityGroup<TestEntityWithComponentViewAndComponentStruct>(fromEgid, toEgid);
+                _scheduler.SubmitEntities();
+            }
+
+            Assert.Throws<ECSException>(SwapEntityAlreadyExists, "When target EGID already exists it should throw an exception");
+
+            void SwapEntityNotFound()
+            {
+                fromEgid = new EGID(3, GroupA);
+                toEgid = new EGID(3, GroupB);
+                _functions.SwapEntityGroup<TestEntityWithComponentViewAndComponentStruct>(fromEgid, toEgid);
+                _scheduler.SubmitEntities();
+            }
+
+            Assert.Throws<ECSException>(SwapEntityAlreadyExists, "When source EGID doesn't exists it should throw an exception");
+        }
+
+        EntityComponentInitializer CreateTestEntity(uint entityId, ExclusiveGroupStruct group, int value = 1)
+        {
+            var initializer = _factory.BuildEntity<TestEntityWithComponentViewAndComponentStruct>
+                (entityId, group, new object[] {new TestFloatValue(value), new TestIntValue(value)});
+            initializer.Init(new TestEntityStruct(value, value));
+            return initializer;
         }
 
         SimpleEntitiesSubmissionScheduler _scheduler;
