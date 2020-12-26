0ff218c90e9e6de1ea7cbbcc1adecf3d69c5fa9f
sebas77 <ssense@tiscali.it>
2020-12-26 16:25:41 +0000

    add ValueReference

--- Svelto.ECS.Tests/ECS/SveltoTests.cs
+++ Svelto.ECS.Tests/ECS/SveltoTests.cs
@@ -833,15 +833,6 @@ namespace Svelto.ECS.Tests.Messy
         public Transform(int i) { value = i; }
     }
 
-    struct ValueReference<T>
-    {
-        public ValueReference(Object obj) { _pointer = GCHandle.Alloc(obj, GCHandleType.Normal); }
-
-        public static implicit operator T(ValueReference<T> t) => (T) t._pointer.Target;
-
-        GCHandle _pointer;
-    }
-
     class TestIt : ITestIt
     {
         Transform _reference;
