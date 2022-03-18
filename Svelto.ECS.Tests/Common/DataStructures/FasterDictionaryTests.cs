0512beed4fd9facd376dfb62974f4765db2d21e7
Maxwell Keonwoo Kang <code.athei@gmail.com>
2022-03-18 12:29:42 -0700

    update intersect / exclude / union test case

--- Svelto.ECS.Tests/Common/DataStructures/FasterDictionaryTests.cs
+++ Svelto.ECS.Tests/Common/DataStructures/FasterDictionaryTests.cs
@@ -17,5 +17,77 @@ namespace Svelto.Common.Tests.Datastructures
 
             Assert.Throws(typeof(SveltoDictionaryException), TestAddDup);
         }
+
+        [TestCase]
+        public void TestIntersect()
+        {
+            FasterDictionary<int, int> test1 = new FasterDictionary<int, int>();
+            FasterDictionary<int, int> test2 = new FasterDictionary<int, int>();
+
+            for (int i = 0; i < 100; ++i)
+                test1.Add(i, i);
+
+            for (int i = 0; i < 200; i += 2)
+                test2.Add(i, i);
+
+            test1.Intersect(test2);
+
+            for (int i = 0; i < test1.count; ++i)
+                Console.LogDebug(test1.unsafeKeys[i].key.ToString());
+
+            Assert.AreEqual(50, test1.count);
+
+            for (int i = 0; i < 100; i += 2)
+                Assert.IsTrue(test1.ContainsKey(i));
+        }
+
+        [TestCase]
+        public void TestExclude()
+        {
+            FasterDictionary<int, int> test1 = new FasterDictionary<int, int>();
+            FasterDictionary<int, int> test2 = new FasterDictionary<int, int>();
+
+            for (int i = 0; i < 100; ++i)
+                test1.Add(i, i);
+
+            for (int i = 0; i < 200; i += 2)
+                test2.Add(i, i);
+
+            test1.Intersect(test2);
+
+            for (int i = 0; i < test1.count; ++i)
+                Console.LogDebug(test1.unsafeKeys[i].key.ToString());
+
+            Assert.AreEqual(50, test1.count);
+
+            for (int i = 1; i < 100; i += 2)
+                Assert.IsTrue(test1.ContainsKey(i));
+        }
+
+        [TestCase]
+        public void TestUnion()
+        {
+            FasterDictionary<int, int> test1 = new FasterDictionary<int, int>();
+            FasterDictionary<int, int> test2 = new FasterDictionary<int, int>();
+
+            for (int i = 0; i < 100; ++i)
+                test1.Add(i, i);
+
+            for (int i = 0; i < 200; i += 2)
+                test2.Add(i, i);
+
+            test1.Union(test2);
+
+            for (int i = 0; i < test1.count; ++i)
+                Console.LogDebug(test1.unsafeKeys[i].key.ToString());
+
+            Assert.AreEqual(150, test1.count);
+
+            for (int i = 0; i < 100; i++)
+                Assert.IsTrue(test1.ContainsKey(i));
+
+            for (int i = 100; i < 200; i += 2)
+                Assert.IsTrue(test1.ContainsKey(i));
+        }
     }
 }
